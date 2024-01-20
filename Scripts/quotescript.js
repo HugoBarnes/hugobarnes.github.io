8b32679acb4bc46c48f72a0e06966b2f71af15bd
HugoBarnes <hugo.s.barnes@gmail.com>
2024-01-20 00:35:10 -0500

    working on and finishing quote generator

--- Scripts/quotescript.js
+++ Scripts/quotescript.js
@@ -3,3 +3,19 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = '../index.html';
     });
 });
+$(document).ready(function(){
+    var category = 'love';
+    $.ajax({
+        method:'GET',
+        url:'https://api.api-ninjas.com/v1/quotes?category=' + category,
+        headers: { 'X-Api-Key': '+7c4/ENzhG3/tnJFqg4AGg==JdiiuHgKQwwPru1Q'},
+        contentType: 'application/json',
+        success: function(result) {
+            $('#quote').html(result[0].quote);
+            $('#author').html(result[0].author);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error ', jqXHR.responseText);
+        }
+    });
+});
